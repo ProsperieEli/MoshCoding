be3a7b26fdb501fff5313620552bea86411bf6b9
ElijahP <ElijahProsperie@gmail.com>
2022-06-05 04:52:49 -0700

    added tag list

--- src/Projects/NumberCounter/counter.jsx
+++ src/Projects/NumberCounter/counter.jsx
@@ -2,18 +2,33 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     }
-    render() { 
 
-        const classes = "badge m-2";
-        // classes += (this.state.count === 0) ? "text-warmomh"
-        return (
-            <> 
-            <span className='badge border 4px m-2 text-warning' >{this.formatCount()}</span>
+renderTags() {
+    if (this.state.tags === 0) return <p>there are no tags</p>;
+            return <ul>
+            {this.state.tags.map((tag)=> <li key={tag}>{tag}</li> )}
+            </ul>
+
+}
+
+render() { 
+    return (
+        <> 
+            <span className={this.getBadgeClasses} >{this.formatCount()}</span>
             <button className='btn-secondary tn-sm'>Incrementing</button>
+            {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
         </>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = 'badge border 4px m-2 text-';
+        classes += this.state.count === 0 ? "warning" : 'primary';
+        return classes;
     }
 
     formatCount() {
